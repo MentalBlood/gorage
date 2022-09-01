c92496a934aa7b71f21c5a2777c4ea7860e8876e
Нечепоренко Степан <neceporenkostepan@gmail.com>
2022-09-01 12:36:02 +0300

    fix

--- include/MemoryStorage.hpp
+++ include/MemoryStorage.hpp
@@ -12,30 +12,60 @@
 #include "Storage.hpp"
 
 
-
+/**
+ * @brief Storage for arbitrary objects, uses std::unordered_map
+ * 
+ * @tparam T Objects type
+ */
 template<class T>
-class MemoryStorage : public Storage<T> {
+class gorage::MemoryStorage : public Storage<T> {
 
 public:
 
+	/**
+	 * @brief Construct a new Memory Storage object
+	 * 
+	 */
 	MemoryStorage() {}
 
 protected:
 
+	/**
+	 * @brief Saves given object with given USI
+	 * 
+	 * @param usi Unique Storage Identifier
+	 * @param object Arbitrary object
+	 */
 	void save(const std::string& usi, const T& object) {
 		this->storage[usi] = object;
 	}
 
+	/**
+	 * @brief Loads object with given USI
+	 * 
+	 * @param usi Unique Storage Identifier
+	 * @return T Object type
+	 */
 	T load(const std::string& usi) {
 		return this->storage.at(usi);
 	}
 
+	/**
+	 * @brief Removes object with given USI
+	 * 
+	 * @param usi Unique Storage Identifier
+	 */
 	void remove(const std::string& usi) {
 		this->storage.at(usi);
 		this->storage.erase(usi);
 	}
 
 private:
+
+	/**
+	 * @brief Map to store objects in
+	 * 
+	 */
 	std::unordered_map<std::string, T> storage;
 
 };
