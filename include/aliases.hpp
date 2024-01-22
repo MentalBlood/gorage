e2654bdd9b36757352ed5659cf6b0e4d98fb147b
necheporenko_s_iu <necheporenko_s_iu@goznak.ru>
2024-01-22 10:26:17 +0300

    add alias for filesystem based indexes creation

--- include/aliases.hpp
+++ include/aliases.hpp
@@ -1,28 +1,45 @@
+#include "File.hpp"
 #include "MemoryStorage.hpp"
 #include "Storage.hpp"
+#include <filesystem>
 
 namespace gorage {
-template <class T, class S = gorage::MemoryStorage<T>>
+template <class T>
 void indexes(gorage::Storage<T> &storage,
              const std::map<std::string, typename gorage::Storage<T>::Index::Extractor::V> &functions) {
-  if constexpr (std::is_same_v<S, gorage::MemoryStorage<T>>) {
-    auto indexes = std::map<std::string, typename gorage::Storage<T>::Index>();
-    for (const auto &n_f : functions)
-      indexes[n_f.first] = typename gorage::Storage<T>::Index(
-          n_f.second, std::make_shared<gorage::MemoryStorage<gorage::Pointer<gorage::Storage<gorage::Key>>>>(),
-          std::make_shared<gorage::MemoryStorage<gorage::Pointer<gorage::Storage<std::string>>>>(),
-          [](const std::string &value) {
-            return gorage::Pointer<gorage::Storage<gorage::Key>>(
-                std::make_shared<gorage::MemoryStorage<gorage::Key>>());
-          },
-          [](const gorage::Key &key) {
-            return gorage::Pointer<gorage::Storage<std::string>>(
-                std::make_shared<gorage::MemoryStorage<std::string>>());
-          });
-    storage.indexes(indexes);
-  }
+  auto indexes = std::map<std::string, typename gorage::Storage<T>::Index>();
+  for (const auto &n_f : functions)
+    indexes[n_f.first] = typename gorage::Storage<T>::Index(
+        n_f.second, std::make_shared<gorage::MemoryStorage<gorage::Pointer<gorage::Storage<gorage::Key>>>>(),
+        std::make_shared<gorage::MemoryStorage<gorage::Pointer<gorage::Storage<std::string>>>>(),
+        [](const std::string &value) {
+          return gorage::Pointer<gorage::Storage<gorage::Key>>(std::make_shared<gorage::MemoryStorage<gorage::Key>>());
+        },
+        [](const gorage::Key &key) {
+          return gorage::Pointer<gorage::Storage<std::string>>(std::make_shared<gorage::MemoryStorage<std::string>>());
+        });
+  storage.indexes(indexes);
 }
-template <class T, class S = gorage::MemoryStorage<T>> void indexes(gorage::Storage<T> &storage) {
+template <class T>
+void indexes(gorage::Storage<T> &storage,
+             const std::map<std::string, typename gorage::Storage<T>::Index::Extractor::V> &functions,
+             const std::filesystem::path &path) {
+  auto indexes = std::map<std::string, typename gorage::Storage<T>::Index>();
+  for (const auto &n_f : functions)
+    indexes[n_f.first] = typename gorage::Storage<T>::Index(
+        n_f.second, std::make_shared<gorage::MemoryStorage<gorage::Pointer<gorage::Storage<gorage::Key>>>>(),
+        std::make_shared<gorage::MemoryStorage<gorage::Pointer<gorage::Storage<std::string>>>>(),
+        [path](const std::string &value) {
+          return gorage::Pointer<gorage::Storage<gorage::File<gorage::Key>>>(
+              std::make_shared<gorage::Storage<gorage::File<gorage::Key>>>(path / "value_to_keys" / value));
+        },
+        [path](const gorage::Key &key) {
+          return gorage::Pointer<gorage::Storage<gorage::File<std::string>>>(
+              std::make_shared<gorage::Storage<gorage::File<std::string>>>(path / "key_to_values" / key.value));
+        });
+  storage.indexes(indexes);
+}
+template <class T> void indexes(gorage::Storage<T> &storage) {
   if (!std::is_base_of<gorage::Json, T>::value)
     throw exceptions::NotImplemented("Automatic field indexes for non-JSON objects is not implemented");
 
@@ -30,6 +47,6 @@ template <class T, class S = gorage::MemoryStorage<T>> void indexes(gorage::Stor
   for (const auto &field : Json::cast<Json::Dict>(T().structure()))
     fields[field.first] = field.first;
 
-  indexes<T, S>(storage, fields);
+  indexes<T>(storage, fields);
 }
 } // namespace gorage
